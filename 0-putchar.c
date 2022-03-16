515e256ac6596cb396cebe55d35049972919fb05
estifanos <berhanestifanos@gmail.com>
2022-03-16 10:49:35 -0700

    Low level programming

---
+++ 0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * MAIN -ENTRY POINT
+ *RETURN: 0 (SUCCESS)
+ **/
+int main(void)
+{
+	char a[] = "_putchar";
+	int c;
+
+	for (c=0;c<8;c++)
+	{
+	_putchar(a[c]);
+	}
+	_putcahr('\n');
+	return (0);
+}
